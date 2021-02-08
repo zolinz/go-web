3ebd1834dba2cdc14ff52d93b3c4412bc872c390
Zoltan Kovacs <>
2021-02-08 21:07:59 +1100

    blah 3

--- cmd/main.go
+++ cmd/main.go
@@ -39,7 +39,7 @@ func loadPage(title string) (*Page, error) {
 	}
 	return &Page{Title: title, Body: body}, nil
 }
-//view handler function now again
+//view handler function now again again
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
